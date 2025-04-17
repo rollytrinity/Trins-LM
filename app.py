b6a37d5c81c8db1e972aee7a182d6531c68fe013
rollytrinity <gullandtrinity@gmail.com>
2025-04-17 11:39:51 +0100

    testing basic imports

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import os
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
 
 st.title("✅ Hello Streamlit!")
 st.write("If you see this, the app is running properly!")
